a438aaf7b2edb1393789420b96e905b46574a9ef
Tyler Lesmann <redhatcat@gmail.com>
2010-09-10 13:56:00 -0600

    Licenses now easily managed.

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -2,7 +2,7 @@ class LicensesController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all, :except => :index
+  auto_actions :create, :destroy
   auto_actions_for :plugin, [:new, :create]
 
 end
