2dd8de82463742cd88dc2e2d36db868556ed56ce
Tyler Lesmann <redhatcat@gmail.com>
2010-09-28 13:56:49 -0600

    Widgets & descendants editable from edit screen

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -2,7 +2,7 @@ class ParametersController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all, :except => :index
+  auto_actions :create, :destroy
   auto_actions_for :widget, [:create]
 
 end
--- app/controllers/valid_values_controller.rb
+++ app/controllers/valid_values_controller.rb
@@ -2,7 +2,7 @@ class ValidValuesController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all, :except => :index
+  auto_actions :create, :destroy
   auto_actions_for :parameter, [:create]
 
 end
