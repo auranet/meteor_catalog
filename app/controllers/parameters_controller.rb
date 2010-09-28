03acc8db6a517068ac848cefa140d36b69219e2f
Tyler Lesmann <redhatcat@gmail.com>
2010-09-28 14:10:45 -0600

    Inline editing for parameters.  Valid values soon.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -2,7 +2,7 @@ class ParametersController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :create, :destroy
+  auto_actions :create, :destroy, :update
   auto_actions_for :widget, [:create]
 
 end
--- app/controllers/valid_values_controller.rb
+++ app/controllers/valid_values_controller.rb
@@ -2,7 +2,7 @@ class ValidValuesController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :create, :destroy
+  auto_actions :create, :destroy, :update
   auto_actions_for :parameter, [:create]
 
 end
