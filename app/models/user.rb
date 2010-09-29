f0561d7de4cd9d54e98edf59d6977fb894c64440
Tyler Lesmann <redhatcat@gmail.com>
2010-09-29 17:49:34 -0600

    Sign-up disabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,17 +13,17 @@ class User < ActiveRecord::Base
   # Just remove it if you don't want that
   before_create { |user| user.administrator = true if !Rails.env.test? && count == 0 }
 
-  
+
   # --- Signup lifecycle --- #
 
   lifecycle do
 
     state :active, :default => true
 
-    create :signup, :available_to => "Guest",
-           :params => [:name, :email_address, :password, :password_confirmation],
-           :become => :active
-             
+    #create :signup, :available_to => "Guest",
+    #       :params => [:name, :email_address, :password, :password_confirmation],
+    #       :become => :active
+
     transition :request_password_reset, { :active => :active }, :new_key => true do
       UserMailer.deliver_forgot_password(self, lifecycle.key)
     end
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
                :params => [ :password, :password_confirmation ]
 
   end
-  
+
 
   # --- Permissions --- #
 
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   end
 
   def update_permitted?
-    acting_user.administrator? || 
+    acting_user.administrator? ||
       (acting_user == self && only_changed?(:email_address, :crypted_password,
                                             :current_password, :password, :password_confirmation))
     # Note: crypted_password has attr_protected so although it is permitted to change, it cannot be changed
